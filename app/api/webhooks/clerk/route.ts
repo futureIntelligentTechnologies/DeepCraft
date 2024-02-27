f35bd4cb3e81f7143bdceafddfe0d97327f56aab
Bhabishya Luitel <bhabishyaluitel@gmail.com>
2024-02-28 02:00:01 +0545

    Webhooks and database configuration

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,4 +1,4 @@
-/* eslint-disable camelcase */
+  /* eslint-disable camelcase */
 import { clerkClient } from "@clerk/nextjs";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
